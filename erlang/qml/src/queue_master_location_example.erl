c8da16e9e2d926893b707735d6fb5672ba9fd11f
Ayanda Dube <ayanda.dube@erlang-solutions.com>
2018-03-21 22:28:04 +0000

    Add type to QML declaration example

--- erlang/qml/src/queue_master_location_example.erl
+++ erlang/qml/src/queue_master_location_example.erl
@@ -9,13 +9,13 @@ start() ->
 
   %% Start a network connection to any cluster node
   {ok, Connection} = amqp_connection:start(#amqp_params_network{}),
-  
+
   %% Open a channel on the connection
   {ok, Channel}    = amqp_connection:open_channel(Connection),
 
   %% Declare a queue with a queue master location policy
   Queue= <<"microservices.queue.1">>,
-  Args = [{<<"x-queue-master-locator">>, <<"min-masters">>}],
+  Args = [{<<"x-queue-master-locator">>, longstr, <<"min-masters">>}],
   QueueDeclare = #'queue.declare'{queue      = Queue,
                                   auto_delete= true,
                                   durable    = true,
@@ -25,7 +25,7 @@ start() ->
   amqp_channel:subscribe(Channel, #'basic.consume'{queue  = Queue,
                                                    no_ack = true}, self()),
   loop(Channel).
-  
+
 loop(Channel) ->
       receive
           %% This is the first message received
@@ -38,11 +38,11 @@ loop(Channel) ->
 
           %% Message delivery
           {#'basic.deliver'{delivery_tag = Tag}, Content} ->
-		  
+
               %% Do something with the message content...write to file for example
 			  file:write_file( "received_message.txt", Content ),
-			  
-              %% Acknowledge delivery 
+
+              %% Acknowledge delivery
 			  amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = Tag}),
               loop(Channel)
       end.
