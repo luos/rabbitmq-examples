a30cc112e2ec4f1a47b035e3d151a6074a28faaf
Lajos Gerecs <lajos.gerecs@erlang-solutions.com>
2020-10-13 12:45:50 +0200

    add exercise

--- java/exercises/RabbitExerciseChat/src/main/java/com/esl/uk/App.java
+++ java/exercises/RabbitExerciseChat/src/main/java/com/esl/uk/App.java
@@ -50,15 +50,19 @@ public class App {
                 System.out.println("This message could not be routed, user is not online: " + returned.getRoutingKey() + " - " + new String(returned.getBody()));
             });
 
+
             String commonQueueName = NAME + "-common";
 
-            // 1. Declare the common-room exchange, it should be durable, fanout
+            // 1. Declare the common room exchange, it should be durable, fanout
+            channel.exchangeDeclare("common-room", "fanout", true);
 
             // 1. declare an auto delete queue for receiving messages
             // 2. bind the queue to the common-room exchange
 
+            channel.queueDeclare(commonQueueName, true, false, true, null);
 
             // 1. bind the queue to the common-room exchange, routing key can be anything
+            channel.queueBind(commonQueueName, "common-room", "");
 
             // process incoming common-room messages
             channel.basicConsume(commonQueueName, new DefaultConsumer(channel) {
@@ -82,15 +86,15 @@ public class App {
             });
 
             // 1. declare an exchange for private-messages, this should be a direct exchange
+            channel.exchangeDeclare("private-messages", "direct", true);
 
             // 1. declare an exclusive queue for private-messages
             // 2. bind it to the private-messages exchange, routing key should be your name
 
             String privateMessagesQueueName = NAME + "-privates";
+            channel.queueDeclare(privateMessagesQueueName, true, true, true, null);
+            channel.queueBind(privateMessagesQueueName, "private-messages", NAME);
 
-            // uncomment the following lines after the private queuei s declared
-
-/*
             channel.basicConsume(privateMessagesQueueName, new DefaultConsumer(channel) {
                 @Override
                 public void handleDelivery(String consumerTag,
@@ -109,7 +113,7 @@ public class App {
                     System.out.println("[PRIVATE]" + username + ": " + new String(body));
                     this.getChannel().basicAck(envelope.getDeliveryTag(), false);
                 }
-            });*/
+            });
 
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -126,10 +130,11 @@ public class App {
 
                     // 1. build a message with a header "chat-username"
                     // 2. publish it to the common-room exchange
+
                     Map<String, Object> headers = new HashMap<>();
                     headers.put("chat-username", NAME);
                     AMQP.BasicProperties props = new AMQP.BasicProperties.Builder().headers(headers).build();
-                    // add the publish call
+                    channel.basicPublish("common-room", "", props, message.getBytes());
                 } else if (command.toLowerCase().startsWith("private")) {
                     String rest = command.substring(8);
                     Integer nextSpace = rest.indexOf(" ");
